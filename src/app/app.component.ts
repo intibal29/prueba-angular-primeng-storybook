b6bdf7fb483a754bbe379ac2099904065d31c0f0
intissae balouk anakar <ibalouk@araba.eus>
2025-03-12 14:35:20 +0100

    vista funciona pero fea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+
 import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';  // Asegúrate de importar FormsModule
-
+import { InputTextModule } from 'primeng/inputtext';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableModule, CalendarModule, FormsModule],  // Incluye FormsModule y CalendarModule
+  imports: [RouterOutlet, TabMenuModule,ButtonModule,TableModule,InputTextModule, CalendarModule, FormsModule],  // Incluye FormsModule y CalendarModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'nuevo-proyecto';
-  cars = [
-    { brand: 'Toyota', year: 2020 },
-    { brand: 'Honda', year: 2021 },
-    { brand: 'Ford', year: 2022 }
-  ];
-  date: Date = new Date();  // Inicializa la propiedad 'date'
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          { label: 'Dashboard', icon: 'pi pi-home' },
+          { label: 'Transactions', icon: 'pi pi-chart-line' },
+          { label: 'Products', icon: 'pi pi-list' },
+          { label: 'Messages', icon: 'pi pi-inbox' }
+      ]
+  }
+  value: string | undefined;
+  date1: Date = new Date();  // Inicializa la propiedad 'date'
+  date2: Date = new Date();  // Inicializa la propiedad 'date'
+  date3: Date = new Date();  // Inicializa la propiedad 'date'
 }
