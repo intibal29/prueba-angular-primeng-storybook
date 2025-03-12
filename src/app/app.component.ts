585a819bf342955146f9c62304a2ac512d4e3540
intibal29 <117351402+intibal29@users.noreply.github.com>
2025-03-12 23:01:46 +0100

    estructura del html mejorada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,32 +3,43 @@ import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { TabMenuModule } from 'primeng/tabmenu';
-
 import { CalendarModule } from 'primeng/calendar';
-import { FormsModule } from '@angular/forms';  // Asegúrate de importar FormsModule
+import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenuItem } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabMenuModule,ButtonModule,TableModule,InputTextModule, CalendarModule, FormsModule],  // Incluye FormsModule y CalendarModule
+  imports: [
+    RouterOutlet, 
+    TabMenuModule, 
+    ButtonModule, 
+    TableModule, 
+    InputTextModule, 
+    CalendarModule, 
+    FormsModule, 
+    CardModule, 
+    DividerModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nuevo-proyecto';
-  items: MenuItem[] | undefined;
+  title = 'Nuevo Proyecto';
+  items: MenuItem[] = [];
 
   ngOnInit() {
-      this.items = [
-          { label: 'Dashboard', icon: 'pi pi-home' },
-          { label: 'Transactions', icon: 'pi pi-chart-line' },
-          { label: 'Products', icon: 'pi pi-list' },
-          { label: 'Messages', icon: 'pi pi-inbox' }
-      ]
+    this.items = [
+      { label: 'Dashboard', icon: 'pi pi-home' },
+      { label: 'Transactions', icon: 'pi pi-chart-line' },
+      { label: 'Products', icon: 'pi pi-list' },
+      { label: 'Messages', icon: 'pi pi-inbox' }
+    ];
   }
-  value: string | undefined;
-  date1: Date = new Date();  // Inicializa la propiedad 'date'
-  date2: Date = new Date();  // Inicializa la propiedad 'date'
-  date3: Date = new Date();  // Inicializa la propiedad 'date'
+
+  value: string = '';
+  date1: Date = new Date();
 }
